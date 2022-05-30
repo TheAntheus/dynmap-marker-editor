69b2fd6101fc477264a566ec805bf4de567edd13
Robert Jobe <TheAntheus@users.noreply.github.com>
2022-05-30 04:44:27 -0500

    Added logger for future use

--- src/markers.py
+++ src/markers.py
@@ -1,16 +1,38 @@
 import yaml
+import logging
 
 # Program Information Variables
 # Variables should be lowercase with underscores between words
 _program = "Dynmap Markers Editor"
 _author = "Robert Jobe"
 _version = 1.0
-_startYear = 2022  # Do not change
-_latestYear = 2022
+_start_year = 2022  # Do not change
+_latest_year = 2022
+
+
+# Setup logger
+logger = logging.getLogger('Logger')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# Use logger.[debug/info/warning/error/critical] to log to the console.
+
 
 #  Print program information to console
 print("%s version %.1f" % (_program, _version))
-print("Copyright %i-%i %s. All Rights Reserved\n" % (_startYear, _latestYear, _author))
+print("Copyright %i-%i %s. All Rights Reserved\n" % (_start_year, _latest_year, _author))
 
 
 def get_data():
